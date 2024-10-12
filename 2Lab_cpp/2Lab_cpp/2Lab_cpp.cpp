0c707816cd521bd3335cba31a7bda3034c0c9a7f
XL1TTE <zzzxl1tteqqq@mail.com>
2024-10-12 18:58:32 +0700

    2lab_cpp

---
+++ 2Lab_cpp/2Lab_cpp/2Lab_cpp.cpp
@@ -0,0 +1,54 @@
+#include "ForwardList.h"
+#include "random"
+#include <iostream>
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    ForwardList<int> my_list = ForwardList<int>();
+
+    my_list.PushBack(10);
+
+    my_list[0] = 5;
+
+
+    cout << my_list.GetByIndex(0) << endl;
+    my_list.PushFront(19);
+    cout << my_list.GetFirst() << endl;
+    cout << my_list.GetLast() << endl;
+    cout << my_list.Size() << endl;
+    my_list.PushBack(10);
+    cout << my_list.GetLast() << endl;
+    my_list.RemoveLast();
+
+    srand(time(0));
+    for (int i = 0; i < 5; i++) {
+        my_list.PushBack(rand() % 100);
+    }
+    cout << endl << "Iteration Testing" << endl;
+    int index = 0;
+    for (auto it = my_list.begin(); it != my_list.end(); ++it) {
+        cout << index << ". "<< it.item() << endl;
+        index++;
+    }
+
+    my_list.Insert(100, 0);
+    my_list.Insert(-100, 5);
+
+    index = 0;
+    cout << endl << "Insertion Testing" << endl;
+    for (auto it = my_list.begin(); it != my_list.end(); ++it) {
+        cout << index << ". " << it.item() << endl;
+        index++;
+    }
+
+    _CrtDumpMemoryLeaks();
+}
+
---
+++ 2Lab_cpp/2Lab_cpp/ForwardList.h
@@ -0,0 +1,223 @@
+#pragma once
+#include "stdexcept"
+template <typename Type>
+class ForwardList {
+
+private:
+    struct Node;
+    Node* _head;
+    Node* _end;
+
+public:
+    class Iterator {
+    public:
+        Iterator(Node* node) : _node(node) {}
+
+        Type& item() const{
+            return this->_node->_item;
+        }
+
+        bool operator==(const Iterator& ToCompare) const {
+            return _node == ToCompare._node;
+        }
+
+        bool operator!=(const Iterator& ToCompare) const {
+            return !(*this == ToCompare);
+        }
+
+        Type& operator*() const {
+            if (_node) {
+                return _node->_item;
+            }
+            return;
+        }
+
+        Iterator& operator++() {
+            if (_node) {
+                _node = _node->_next;
+            }
+            return *this;
+        }
+    private:
+        Node* _node;
+    };
+
+public:
+
+    ForwardList();
+    ~ForwardList();
+
+
+    void PushFront(const Type& item);
+    void PushBack(const Type& item);
+    void Insert(const Type& item, const size_t index);
+
+    void RemoveLast();
+    void RemoveFirst();
+
+    Type GetFirst() const;
+    Type GetLast() const;
+
+    Type& GetByIndex(size_t index) const;
+
+    Type& operator[](size_t index) const;
+
+    Iterator begin() const;
+
+    Iterator end() const;
+
+    size_t Size() const;
+
+private:
+    bool is_empty() const{
+        return (_head == nullptr);
+    }
+
+private:
+    struct Node
+    {
+        Node() : _next(nullptr) {}
+
+        Node(const Type& item) : _item(item), _next(nullptr) {}
+
+        Type _item;
+        Node* _next;
+    };
+};
+
+template<typename Type>
+ForwardList<Type>::ForwardList() : _head(nullptr), _end(nullptr) {}
+
+template<typename Type>
+ForwardList<Type>::~ForwardList() {
+    while (!is_empty()) {
+        RemoveFirst();
+    }
+}
+
+template<typename Type>
+void ForwardList<Type>::PushFront(const Type& item) {
+    Node* node = new Node(item);
+    if (is_empty()) {
+        _head = _end = node;
+        return;
+    }
+    node->_next = _head;
+    _head = node;
+}
+template<typename Type>
+void ForwardList<Type>::PushBack(const Type& item) {
+    Node* node = new Node(item);
+    if (is_empty()) {
+        _head = node;
+        _end = node;
+        return;
+    }
+    _end->_next = node;
+    _end = node;
+}
+
+template<typename Type>
+void ForwardList<Type>::Insert(const Type& item, const size_t index) {
+    if (index > Size() - 1 || index < 0) {
+        throw std::out_of_range("Индекс выходит за пределы списка");
+    }
+    if (index == 0) {
+        PushFront(item);
+        return;
+    }
+    Node* node = new Node(item);
+    Node* current = _head;
+    for (int i = 0; i < index-1; i++) {
+        current = current->_next;
+    }
+    node->_next = current->_next;
+    current->_next = node;
+}
+
+template<typename Type>
+void ForwardList<Type>::RemoveLast() {
+    if (is_empty()) {
+        return;
+    }
+    if (_head == _end) {
+        delete _head;
+        _head = _end = nullptr;
+        return;
+    }
+    Node* current = _head;
+    while (current->_next != _end) {
+        current = current->_next;
+    }
+    delete _end;
+    _end = current;
+    _end->_next = nullptr;
+}
+
+template<typename Type>
+void ForwardList<Type>::RemoveFirst() {
+    if (is_empty()) {
+        return;
+    }
+    Node* temp = _head;
+    _head = _head->_next;
+    delete temp;
+    if (_head == nullptr) {
+        _end = nullptr;
+    }
+}
+
+template<typename Type>
+Type ForwardList<Type>::GetFirst() const {
+    if (is_empty()) {
+        throw std::runtime_error("Список пуст");
+    }
+    return _head->_item;
+}
+
+template<typename Type>
+Type ForwardList<Type>::GetLast() const {
+    if (is_empty()) {
+        throw std::runtime_error("Списко пуст");
+    }
+    return _end->_item;
+}
+
+template<typename Type>
+Type& ForwardList<Type>::GetByIndex(size_t index) const {
+    if (index > Size() - 1 || index < 0) {
+        throw std::out_of_range("Индекс выходит за пределы списка");
+    }
+    Node* current = _head;
+    for (size_t i = 0; i < index; ++i) {
+        current = current->_next;
+    }
+    return current->_item;
+}
+
+template<typename Type>
+Type& ForwardList<Type>::operator[](size_t index) const{
+    return GetByIndex(index);
+}
+
+
+
+template<typename Type>
+typename ForwardList<Type>::Iterator ForwardList<Type>::begin() const {
+    return Iterator(_head);
+}
+
+template<typename Type>
+typename ForwardList<Type>::Iterator ForwardList<Type>::end() const {
+    return Iterator(nullptr);
+}
+template<typename Type>
+size_t ForwardList<Type>::Size() const {
+    size_t size = 0;
+    for (Iterator current = begin(); current != end(); ++current) {
+        ++size;
+    }
+    return size;
+}
+
+
